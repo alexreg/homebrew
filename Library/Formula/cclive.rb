dc74868a39c4724ec501234d1c484e1e751e404f
Alex Regueiro <alex@noldorin.com>
2014-01-21 17:08:57 +0000

    cclive: added devel and head builds

--- Library/Formula/cclive.rb
+++ Library/Formula/cclive.rb
@@ -5,6 +5,13 @@ class Cclive < Formula
   url 'http://downloads.sourceforge.net/project/cclive/0.7/cclive-0.7.16.tar.xz'
   sha1 '2bdee70f5e2026165ca444a306bb76fc5ede97b4'
 
+  devel do
+    url 'http://downloads.sourceforge.net/project/cclive/0.9/cclive-0.9.3.tar.xz'
+    sha1 'bdcc87b3f03ed24dfe39a06159b565df7fdce875'
+  end
+
+  head 'https://github.com/legatvs/cclive.git'
+
   conflicts_with 'clozure-cl', :because => 'both install a ccl binary'
 
   depends_on 'pkg-config' => :build
@@ -12,6 +19,13 @@ class Cclive < Formula
   depends_on 'quvi'
   depends_on 'boost'
   depends_on 'pcre'
+  if build.devel?
+    depends_on 'glibmm' => :build
+    depends_on 'glib' => :build
+    depends_on 'libsigc++' => :build
+  end
+
+  # OLD FORMULA: https://github.com/Homebrew/homebrew/pull/22276/files
 
   def install
     system "./configure", "--disable-dependency-tracking",
